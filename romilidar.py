97eb0e31066f694e32a1134a538a96e769c5a273
Chris Anderson <gca2@earthlink.net>
2020-01-26 19:32:24 -0800

    Got the basics working
    What a hassle dealing with Python 2/3 stuff

--- romilidar.py
+++ romilidar.py
@@ -1,26 +1,45 @@
 from romipi_astar.romipi_driver import AStar
 import time
 import PyLidar3
+import math
+import threading
 
 romi = AStar()
+
+speed = 0.1  # speed in m/s
+direction = 0.0 # direction in radians
+longest = 0
+longest_angle = 0
 linear_ms = 0.0
 rotate_rads = 0.0
-
+x=[]
+y=[]
+for _ in range(360):
+    x.append(0)
+    y.append(0)
 
 port = "/dev/ttyUSB0" #linux
 Obj = PyLidar3.YdLidarX4(port) #PyLidar3.your_version_of_lidar(port,chunk_size)
 
 if(Obj.Connect()):
-    print(Obj.GetDeviceInfo())
-    gen = Obj.StartScanning()
-    t = time.time() # start time
-    while True: 
-        data = next(gen)
-        for angle in range(0,360):
-            if(data[angle]>1000):
-                x[angle] = data[angle] * math.cos(math.radians(angle))
-                y[angle] = data[angle] * math.sin(math.radians(angle))
-    Obj.StopScanning()
-    Obj.Disconnect()
+    try:
+        print(Obj.GetDeviceInfo())
+        gen = Obj.StartScanning()
+        t = time.time() # start time
+        while True:
+            data = next(gen)
+            longest = 0
+            longest_angle = 0
+            for angle in range(0,360):
+                if(data[angle]>10):
+                    if data[angle] > longest:
+                        longest = data[angle]
+                        longest_angle = angle
+            print("longest range, angle", longest, longest_angle)
+            romi.twist(speed, direction)
+    except (KeyboardInterrupt, SystemExit):
+        print("Quitting")
+        Obj.StopScanning()
+        Obj.Disconnect()
 else:
     print("Error connecting to device")
