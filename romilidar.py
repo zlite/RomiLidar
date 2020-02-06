cffc12986114550226586da13cafb48dfc85623c
Chris Anderson <gca2@earthlink.net>
2020-02-05 22:13:19 -0800

    Added binning

--- romilidar.py
+++ romilidar.py
@@ -14,10 +14,12 @@ linear_ms = 0.0
 rotate_rads = 0.0
 x=[]
 y=[]
+bins=[]
 for _ in range(360):
     x.append(0)
     y.append(0)
-
+for _ in range (37):  # clear the bins
+    bins.append(0)
 port = "/dev/ttyUSB0" #linux
 Obj = PyLidar3.YdLidarX4(port) #PyLidar3.your_version_of_lidar(port,chunk_size)
 
@@ -29,13 +31,25 @@ if(Obj.Connect()):
         while True:
             data = next(gen)
             longest = 0
-            longest_angle = 0
+            longest_bin = 0
+            counter = 1
+            for bin_num in range (37):  # clear the bins
+                bins[bin_num] = 0
+            bin_num = 0
             for angle in range(0,360):
-                if(data[angle]>10):
-                    if data[angle] > longest:
-                        longest = data[angle]
-                        longest_angle = angle
-            print("longest range, angle", longest, longest_angle)
+                if(data[angle]> 0):
+                    if counter < 10:
+                        bins[bin_num] = bins[bin_num] + data[angle]
+                        counter = counter + 1
+                    else:
+                        bins[bin_num] = bins[bin_num] + data[angle]
+                        bin_num = bin_num +1
+                        counter = 1
+            for bin_num in range(37):
+                if bins[bin_num] > longest:
+                    longest = bins[bin_num]
+                    longest_bin = bin_num
+            print("longest range bin, bin #", round(longest/1000,0), longest_bin)
             direction = math.radians(longest_angle)
             romi.twist(speed, direction)
     except (KeyboardInterrupt, SystemExit):
