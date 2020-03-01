5b37f220d097f52d447f4c890329d690a6175faf
Chris Anderson <gca2@earthlink.net>
2020-03-01 15:35:02 -0800

    Added bias torward most distant side

--- romilidar.py
+++ romilidar.py
@@ -12,13 +12,14 @@ longest = 0
 longest_angle = 0
 linear_ms = 0.0
 rotate_rads = 0.0
+bintotal = 37  # how many bins there are
 x=[]
 y=[]
 bins=[]
 for _ in range(360):
     x.append(0)
     y.append(0)
-for _ in range (37):  # clear the bins
+for _ in range (bintotal):  # clear the bins
     bins.append(0)
 port = "/dev/ttyUSB0" #linux
 Obj = PyLidar3.YdLidarX4(port) #PyLidar3.your_version_of_lidar(port,chunk_size)
@@ -37,7 +38,7 @@ async def main():
         longest = 0
         longest_bin = 0
         counter = 1
-        for bin_num in range (37):  # clear the bins
+        for bin_num in range (bintotal):  # clear the bins
             bins[bin_num] = 0
         bin_num = 0
         for angle in range(0,360):
@@ -49,13 +50,35 @@ async def main():
                     bins[bin_num] = bins[bin_num] + data[angle]
                     bin_num = bin_num +1
                     counter = 1
-        for bin_num in range(37):
+        for bin_num in range(bintotal):
             if bins[bin_num] > longest:
                 longest = bins[bin_num]
                 longest_bin = bin_num
-        print("longest range bin, bin #", round(longest/1000,0), longest_bin)
+        longest = round(longest/1000,1)
+        
+        left_total = 0
+        right_total = 0
+        for x in range(0,longest_bin-1): # sum the left side
+            left_total = left_total + bins[x]
+        left_average = round((left_total/(longest_bin-1))/1000,1)
+
+        for x in range(longest_bin+1,bintotal): # sum the right side
+            right_total = right_total + bins[x]
+        right_average = round((right_total/(bintotal-longest_bin-1))/1000,1)
+
+        print("Longest range",longest)
+        print("Left average", left_average)
+        print("Right average", right_average)
+        if left_average > right_average:
+            furthest_side_dist = left_average
+        else:
+            furthest_side_dist = right_average
+
+        direction = longest_bin + (longest-furthest_side_dist)*0.1
+        print("longest bin #", longest_bin)
+        print("Adjusted direction", direction)
         direction = round(math.radians((longest_bin*10)-180),2)
-        print ("Direction:", direction)
+        print ("Steer:", direction)
         await asyncio.sleep(0.3)
 
 try:
