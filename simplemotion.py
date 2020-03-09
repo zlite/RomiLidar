7e6d742511ed75d520567983ef7f24b4661fba82
Chris Anderson <gca2@earthlink.net>
2020-03-09 07:58:00 -0700

    proper unit test

--- simplemotion.py
+++ simplemotion.py
@@ -1,8 +1,42 @@
 from romipi_astar.romipi_driver import AStar
 import time
-speed = 0.1
-direction = 0
+import math
+import asyncio
+
 romi = AStar()
 
-while True:
-    romi.twist(speed, direction)
+speed = 0.2  # speed in m/s
+direction = 0.0 # direction in radians
+switch = False
+
+async def motors():
+    global direction
+    while True:
+        romi.twist(speed, direction)
+        await asyncio.sleep(0)
+
+async def main():
+    global speed, direction, switch
+    t = time.time() # start time
+    while True:
+        if direction < 1 and not switch:
+            direction += 0.1
+        else:
+            switch = True
+        if direction > 0 and switch:
+            direction -= 0.1
+        else:
+            switch = False
+        print("Direction", direction)
+        await asyncio.sleep(0.3)
+
+try:
+    ioloop = asyncio.get_event_loop()
+    tasks = [
+        ioloop.create_task(main()),
+        ioloop.create_task(motors())
+        ]
+    ioloop.run_until_complete(asyncio.wait(tasks))
+    ioloop.close()
+except (KeyboardInterrupt, SystemExit):
+    print("Quitting")
