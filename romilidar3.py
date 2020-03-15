4858c765ac7b2d3ffcc96a8525bd4ace037a69e3
Chris Anderson <gca2@earthlink.net>
2020-03-15 14:34:33 -0700

    Working!

--- romilidar3.py
+++ romilidar3.py
@@ -30,30 +30,36 @@ def main():
         left_total = 0
         right_total = 0
         center_total = 0
+        bias = 0
         for angle in range(340,360):  # measure the center
             if(data[angle]> 0):
                 center_total = center_total + data[angle]
         for angle in range(0,20):  # measure the center
             if(data[angle]> 0):
                 center_total = center_total + data[angle]
-        center_average = round(center_total/40000,2)        
+        center_average = round(center_total/40,2)
         for angle in range(20,120):  # measure the right side
             if(data[angle]> 0):
                 right_total = right_total + data[angle]
-        right_average = round(right_total/120000,2)
+        right_average = round(right_total/120,2)
         for angle in range(240,340): # measure the left side
             if(data[angle]> 0):
                 left_total = left_total + data[angle]
-        left_average = round(left_total/120000,2)
+        left_average = round(left_total/120,2)
         print("Center average", center_average)
         print("Left average", left_average)
         print("Right average", right_average)
-        if (center_average > right_average) and (center_average > left_average):
-            direction = 0
+        difference = (left_average - right_average)/1000
+        if (center_average > right_average) and (center_average > left_average): # forward is clearer
+            if left_average < 150: # too close to left wall
+                bias = -0.4
+            if right_average < 150: # too close to right wall
+                bias = 0.4
+            direction = 0 + bias
         if (left_average > right_average) and (left_average > center_average):
-            direction = round(direction + (increment_turn*(left_average-right_average)),2)
+            direction = round(direction + (increment_turn*(difference)),2)
         if (right_average > left_average) and (right_average > center_average):
-            direction = round(direction - (increment_turn*(right_average-left_average)),2)
+            direction = round(direction - (increment_turn*(-1*difference)),2)
         if direction > math.pi: direction = math.pi  # avoid overturning
         if direction < -math.pi: direction = -math.pi
         print ("Steer", direction)
